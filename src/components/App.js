1d08e3a4ec4ac07265a2505f0eb100917264da80
Reza sohrbi <rezasohrabi1995@yahoo.com>
2021-09-05 09:33:02 +0430

    Add TodoList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,28 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { removeTodo } from '../redux/todos/todos.actions';
+import React from 'react';
+import { Switch, Route } from 'react-router';
+import MainLayout from '../layouts/Main';
+import ImageList from './ImageList';
+import TodoList from './TodoList';
 
 
-const mapState = (state) => ({
-  todos: state.todosData.todos,
-})
+
 
 const App = (props) => {
-  const {todos} = useSelector(mapState);
-  const dispatch = useDispatch();
-  console.log('todossssssssssssssssssssssss:',todos)
-  const [toggle, setToggle] = useState(false)
 
-  useEffect(() => {
-    console.log('App.js renderd, so useEffect works perfect')
-  }, [])
-
-  const handleRemoveTodo = (id) => {
-    dispatch(removeTodo(id))
-  }
-
-  const handleToggle = () => {
-    setToggle(!toggle)
-  }
   return (
-    <>
-    <header className='header'>
-      <a href='#'>تصاوير</a>
-      <a href='#'>ليست يادداشت ها</a>
-    </header>
-    <div>
-      <button onClick={handleToggle}>toggle message</button>
-      {toggle && (<div>this is description of toggle to test useState hook</div>)}
-    </div>
-      <ul className='todos'>
-          {todos.map(todo => (
-            <li key={todo.id}><div>Name: {todo.name}</div><div>Username: {todo.username}</div><div>website :{todo.website}</div><button onClick={event => handleRemoveTodo(todo.id)}>remove todo</button></li>
-          ))}
-      </ul>
-      </>
+    // <Switch>
+    //   <Route exact path='/' render={() => {
+    //     <MainLayout >
+    //       <TodoList />
+    //     </MainLayout>
+    //   }}/>
+    //   <Route path='/images' render={() => {
+    //     <MainLayout>
+    //       <ImageList />
+    //     </MainLayout>
+    //   }} />
+    // </Switch>
+    <TodoList />
   )
 }
 
