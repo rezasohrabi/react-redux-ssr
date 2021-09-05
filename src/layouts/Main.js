c03853644e5ccb2b3bc6e57d32280e914eb19cb5
Reza sohrbi <rezasohrabi1995@yahoo.com>
2021-09-05 09:47:14 +0430

    Layout the components

---
+++ src/layouts/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MainLayout = ({children}) => {
+    return (
+        <main>
+            <header className='header'>
+                <a href='#'>تصاوير</a>
+                <a href='#'>ليست يادداشت ها</a>
+            </header>
+            <section className='main-content'>
+                {children}
+            </section>
+        </main>
+
+    )
+}
+
+export default MainLayout;
