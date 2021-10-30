8df0a2cec40c636412f7a82ebe0e1f4cdc0f17dd
= <rezasohrabi1995@yahoo.com>
2021-10-30 22:28:20 +0330

    home page route

---
+++ src/pages/HomePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MainLayout from '../layouts/Main';
+
+const HomePage = () => {
+  return <MainLayout>this is home page</MainLayout>;
+};
+
+export default HomePage;
---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import { HomePage } from '../pages';
+const paths = [
+  {
+    path: '/',
+    exact: true,
+    component: HomePage,
+  },
+];
+
+export default paths;
