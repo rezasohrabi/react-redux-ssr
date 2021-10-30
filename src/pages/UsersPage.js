4e33d678f94bb5041f3d040588ed877373180f0d
= <rezasohrabi1995@yahoo.com>
2021-10-30 23:06:09 +0330

    add users page

---
+++ src/pages/UsersPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UsersPage = () => {
+  return (
+      <div>this is users page</div>
+  )
+};
+
+export default UsersPage;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,15 @@
-import { HomePage } from '../pages';
+import { HomePage, UsersPage } from '../pages';
+import UsersPage from '../pages/UsersPage';
 const paths = [
   {
     path: '/',
     exact: true,
     component: HomePage,
   },
+  {
+      path: '/users',
+      component: UsersPage,
+  }
 ];
 
 export default paths;
