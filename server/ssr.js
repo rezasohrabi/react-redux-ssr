923033e02a571466dd4fff01f4a7f27546bda09c
= <rezasohrabi1995@yahoo.com>
2021-11-01 00:02:59 +0330

    Access active route on the server

--- server/ssr.js
+++ server/ssr.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { StaticRouter } from 'react-router-dom';
+import { StaticRouter, matchPath } from 'react-router-dom';
 import rootReducer from '../src/redux/rootReducer';
 import App from '../src/App';
+import routes from '../src/routes';
 
-const ssr = (req) => {
+
+
+const ssr = async (req) => {
+  const activeRoute = routes.find((route) => matchPath(req.url, route));
+
   const store = createStore(rootReducer);
-  const context = {};
+  const context = { initialProps };
   let content = renderToString(
     <Provider store={store}>
       <StaticRouter location={req.url} context={context}>
@@ -18,7 +23,7 @@ const ssr = (req) => {
   );
   const preloadedState = store.getState();
 
-  return { content, preloadedState };
+  return { content, preloadedState, initialProps };
 };
 
 export default ssr;
